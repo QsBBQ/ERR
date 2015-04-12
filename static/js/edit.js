800db23e6c3a5446bbea394013b85e603480bc54
Michael Herman <hermanmu@gmail.com>
2015-04-12 14:26:51 -0600

    updated jquery, frontend

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,14 +1,34 @@
 $(function() {
 
-  $(".checkbox").on('click', function(e) {
+  $(".checkbox").on('click', function() {
     if ($(this).prop('checked')) {
-      console.log("checked")
+      console.log("checked");
       $(this).closest('tr').find('.enable').prop('disabled', false);
     }
     else {
-      console.log("unchecked")
+      console.log("unchecked");
       $(this).closest('tr').find('.enable').prop('disabled', true);
     }
+  });
+
+  $(".form-button").on('click', function(event) {
+    event.preventDefault();
+    // payload
+    console.log($('.enable-status').attr("name"));
+    var data = {
+      statusUpdate: $('.enable-status').val(),
+      commentsUpdate: $('.enable-comments').val()
+    };
+    // post request
+    $.post("users/save", data, function(err, results) {
+      console.log(results);
+      // if (err) {
+      //   $(classname).html("something went wrong");
+      // } else {
+      //   $(classname).html("success!");
+      // }
+    });
+
   });
 
 });
